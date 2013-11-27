d9381478ddf261272491b60171925d82eaecef22
Moin Virani <moin.virani@gmail.com>
2013-11-26 23:15:57 -0500

    Added table and css to style table

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var game = {
+  turn: "X",
+  intstruct: function(str) {
+      $('caption').text(str);
+  }, 
+  begin: function() {
+    // reset things in case there is an existing game to clear
+    $('body').off
+  }
+
+}
