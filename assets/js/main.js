2f23332f475065ef3440dee55fbe343e8d461ca8
Moin Virani <moin.virani@gmail.com>
2013-11-29 12:11:51 -0500

    can now keep track of who's turn it is and check to see if player wins 3 in a row

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,45 @@
-var game = {
-  turn: "X",
-  intstruct: function(str) {
-      $('caption').text(str);
-  }, 
-  begin: function() {
-    // reset things in case there is an existing game to clear
-    $('body').off
-  }
+// create board
 
+var board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+];
+
+// Create variable to store the winning player
+var winningPlayer = 0;
+
+// Keep track of whose turn it is
+var turn = {
+        number: 0,
+        current_player_color: function() {
+                if(this.number % 2 === 0) {
+                        return 1;
+                }
+                else {
+                        return 2;
+                }
+        },
+        change_turn: function(){
+                this.number += 1;
+        }
+};
+
+// Check to see if any of the rows has 3 in a row
+function check_rows() {
+        for (i=0, i < board.length, i++) {
+                var same = true;
+                for (j=0; j < board[i].length; j++) {
+                        if (board[i][j] === 0 || board[i][j] !=== board[i][0]) {
+                                same = false;
+                        }
+                }
+                if (same) {
+                        return same;
+                }
+        }
 }
+
+
+
+
